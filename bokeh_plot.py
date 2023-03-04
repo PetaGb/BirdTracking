5c584b7fcf16a54f62c6b873e2b45ff5a0145aa8
PetaGb <peter.gelec@gmail.com>
2023-03-04 15:01:04 +0100

    adding image to triangle hovertool.

--- bokeh_plot.py
+++ bokeh_plot.py
@@ -1,6 +1,6 @@
 from bokeh.models import GMapOptions, ZoomInTool, ZoomOutTool, TapTool, OpenURL, HoverTool
-import pandas as pd
 from bokeh.plotting import gmap
+import pandas as pd
 from main import api_key
 
 
@@ -11,35 +11,40 @@ longitudes = df["location_longitude"].values
 date = pd.to_datetime(df["timestamp"]).values
 latitude = 47.7352829
 longitude = 8.9074951
-url = 'https://en.wikipedia.org/wiki/White_stork'
+url = "https://en.wikipedia.org/wiki/White_stork"
+#image_path = "/home/peter/Desktop/stork/stork_cropped.png"
+image_path = "https://upload.wikimedia.org/wikipedia/commons/f/f1/White_stork_%28Ciconia_ciconia%29_standing.jpg"
 
 
-def plot(lat, lng, zoom=5, map_type='roadmap'):
+
+
+def plot(lat, lng, zoom=5, map_type="roadmap"):
 
     gmap_options = GMapOptions(lat=lat, lng=lng, map_type=map_type, zoom=zoom)
-    p = gmap(api_key, gmap_options, title='2016 the move of Mirabell',
+    p = gmap(api_key, gmap_options, title="2016 the move of Mirabell",
              width=bokeh_width, height=bokeh_height)
 
     p.add_tools(ZoomInTool(), ZoomOutTool())
 
+    triangle(p)
+    circles(p)
     p.legend.location = "top_left"
     p.legend.title = "Circles legend"
     p.legend.click_policy = "hide"
-    triangle(p)
-    circles(p)
 
     return p
 
 
-def circles(p, ):
+def circles(p):
     red = p.circle(longitudes[:180], latitudes[:180], size=7, alpha=0.5, color='red', legend_label="spring move")
     red_hover = HoverTool(tooltips=[("Latitude", "@y"), ("Longitude", "@x"),
                                     ("Date", "@date_time{%Y-%m-%d %H:%M:%S.%3N}")],
                           renderers=[red],
-                          formatters={'date_time': 'datetime'})
+                          formatters={"date_time": "datetime"})
 
-    blue = p.circle(longitudes[180:], latitudes[180:], size=7, alpha=0.5, color='blue', legend_label="autumn move")
+    blue = p.circle(longitudes[180:], latitudes[180:], size=7, alpha=0.5, color="blue", legend_label="autumn move")
     blue_hover = HoverTool(tooltips=[("latitude", "@y"), ("Longitude", "@x")], renderers=[blue])
+    #("latitude", "@y"), ("Longitude", "@x")
 
     p.add_tools(red_hover, blue_hover)
 
@@ -49,10 +54,16 @@ def circles(p, ):
 def triangle(p):
     black = p.triangle(longitude, latitude, size=15, alpha=0.5, color='black', legend_label="interactive circles",
                        name="black")
-    black_hover = HoverTool(tooltips=[("Name", "Mirabell"), ("URL", f'{url}')], renderers=[black])
+    img_src = f'<img src="{image_path}" height="70", width="70">'
+    tooltip = f'<div>Latitude: {latitude}<br>Longitude: {longitude}<br>Name: Mirabell<br>URL: {url}<br>{img_src}</div>'
+    black_hover = HoverTool(tooltips=tooltip, renderers=[black])
     black_taptool = TapTool(renderers=[black], callback=OpenURL(url=url))
+
     p.add_tools(black_hover, black_taptool)
 
     return black
 
-# TODOS: castom taptool
+
+
+
+
