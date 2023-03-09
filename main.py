4949b350d0c2d9c797d2e7e9a53fb0547b5491ab
PetaGb <peter.gelec@gmail.com>
2023-03-09 16:02:35 +0100

    Cleaning code a little bit.

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import os
 
 import bokeh_plot
 
+
 app = Flask(__name__)
 
 dotenv_path = Path('/.env')
 load_dotenv(dotenv_path=dotenv_path)
 api_key = os.getenv("API_KEY")
-
 lat, lon = 41.2437, 6.0251
 
 
 @app.route("/")
-
 def index():
     p = bokeh_plot.plot(lat, lon)
     script, div = components(p)
