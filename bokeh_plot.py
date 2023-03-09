a0e253a495cca1f89dbbfae1f1410b94f1cd2fe2
PetaGb <peter.gelec@gmail.com>
2023-03-09 16:00:00 +0100

    Creating pink_triangle function

--- bokeh_plot.py
+++ bokeh_plot.py
@@ -9,8 +9,6 @@ df = pd.read_csv("mirabell_2016.csv")
 latitudes = df["location_latitude"].values
 longitudes = df["location_longitude"].values
 date = pd.to_datetime(df["timestamp"]).values
-url = "http://localhost:8000/redirect.html"
-image_path = "https://upload.wikimedia.org/wikipedia/commons/f/f1/White_stork_%28Ciconia_ciconia%29_standing.jpg"
 
 
 def plot(lat, lng, zoom=5, map_type="roadmap"):
@@ -20,8 +18,10 @@ def plot(lat, lng, zoom=5, map_type="roadmap"):
 
     p.add_tools(ZoomInTool(), ZoomOutTool())
 
-    triangle(p)
+    black_triangle(p)
+    pink_triangle(p)
     circles(p)
+
     p.legend.location = "top_left"
     p.legend.title = "Circles legend"
     p.legend.click_policy = "hide"
@@ -43,17 +43,37 @@ def circles(p):
     return red, blue
 
 
-def triangle(p):
+def black_triangle(p):
     latitude = 47.7352829
     longitude = 8.9074951
-    black = p.triangle(longitude, latitude, size=15, alpha=0.5, color='black', legend_label="interactive circles",
+    url = "http://localhost:8000/redirect.html"
+    black_image_path = "https://upload.wikimedia.org/wikipedia/commons/f/f1/White_stork_%28Ciconia_ciconia%29_standing.jpg"
+    black = p.triangle(longitude, latitude, size=15, alpha=0.5, color='black', legend_label="interactive black",
                        name="black")
-    img_src = f'<img src="{image_path}" height="70", width="70">'
+    img_src = f'<img src="{black_image_path}" height="70", width="70">'
     tooltip = f'<div>Latitude: {latitude}<br>Longitude: {longitude}<br>Name: Mirabell<br>URL: {url}<br>{img_src}</div>'
-    black_hover = HoverTool(tooltips=tooltip, renderers=[black])
-    black_taptool = TapTool(renderers=[black],
+    pink_hover = HoverTool(tooltips=tooltip, renderers=[black])
+    pink_taptool = TapTool(renderers=[black],
                             callback=CustomJS(args=dict(url=url), code="""window.location.href = url;"""))
 
-    p.add_tools(black_hover, black_taptool)
+    p.add_tools(pink_hover, pink_taptool)
 
     return black
+
+
+def pink_triangle(p):
+    latitude = 41.610
+    longitude = 0.618
+    url = "http://localhost:80001/redirect_2.html"
+    pink_image_path = "https://streetartutopia.com/wp-content/uploads/2021/03/Street-Art-Mural-of-a-stork-nest-by-street-artist-muralist-and-painter-Oriol-Arumi-in-Lleida-Spain-2-1068x1068.jpg"
+    pink = p.star(longitude, latitude, size=35, alpha=0.5, color='green', legend_label="interactive pink",
+                       name="pink")
+    img_src = f'<img src="{pink_image_path}" height="70", width="70">'
+    tooltip = f'<div>Latitude: {latitude}<br>Longitude: {longitude}<br>Name: Mural<br>URL: {url}<br>{img_src}</div>'
+    pink_hover = HoverTool(tooltips=tooltip, renderers=[pink])
+    pink_taptool = TapTool(renderers=[pink],
+                            callback=CustomJS(args=dict(url=url), code="""window.location.href = url;"""))
+
+    p.add_tools(pink_hover, pink_taptool)
+
+    return pink
